2256421a432db7d005e3ea24d97a221f58a60bc3
logangrube <96147902+logangrube@users.noreply.github.com>
2022-12-07 19:13:53 -0500

    Update Set-Lure-Rank.js

--- Fishing-Frenzy/Set-Lure-Rank.js
+++ Fishing-Frenzy/Set-Lure-Rank.js
@@ -6,7 +6,7 @@ window.prompt = iframe.contentWindow.prompt;
 
 var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode;
 
-var lure = parseInt(prompt('Lure rank value? (1 to 5)')) - 1;
+var lure = parseInt(prompt('Lure rank value? (1 to 5)')) - 5;
 hack.setState({
     lure: lure
 });
